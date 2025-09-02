b3f85167d05c767aff0d4b86740086a494dc69a9
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-02 12:29:34 +0530

    Create test-checkov.groovy

---
+++ test-checkov.groovy
@@ -0,0 +1,19 @@
+pipelineJob('test-checkov') {
+    displayName('Test Checkov Pipeline')
+    description('Pipeline to test Checkov scan using Tomcat-001 Jenkinsfile')
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/Priyanshu498/Tomcat-001.git')
+                        // No credentials 
+                    }
+                    branch('*/main')
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
