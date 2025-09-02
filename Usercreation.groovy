4e8a5bbe244739701d72ef18210fe7b828f84e20
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-02 12:27:18 +0530

    Create Usercreation.groovy

---
+++ Usercreation.groovy
@@ -0,0 +1,23 @@
+// === Folder Structure ===
+folder('COE') {}
+folder('COE/Cluster-User-creation') {}
+
+pipelineJob('COE/Cluster-User-creation/user-creation-2') {
+    displayName('Cluster User Creation - user-creation-2')
+    description('Pipeline job for creating users in the cluster via K8s Jenkinsfile')
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('git@github.com:SheetalNain/Frappe-Helpdesk.git')
+                        credentials('jenkins (ITMS-Shared-Library)') 
+                    }
+                    branch('*/main')
+                }
+            }
+            scriptPath('helpdesk/Jenkinsfile-K8s')
+        }
+    }
+}
