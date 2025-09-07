ef526b6b7be01c62776803078137ed2619a4916c
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-07 21:54:18 +0530

    Create Helm_CD.groovy

---
+++ Helm_CD.groovy
@@ -0,0 +1,18 @@
+pipelineJob('Uniticonpro/Helm-CD') {
+  
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/ot-central-team/UnitEconPro.git')
+                        credentials('dnishad04')   // Jenkins credential ID
+                    }
+                    branch('*/unified-ingestion')
+                }
+            }
+            scriptPath('JenkinsfileCD')
+            lightweight(true)   // enable lightweight checkout
+        }
+    }
+}
