d659023ccd2bb58cc497ecd2047400cff7f9861e
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-07 21:54:54 +0530

    Create img_tag.groovy

---
+++ img_tag.groovy
@@ -0,0 +1,20 @@
+pipelineJob('Uniticonpro/img-tag-ci') {
+    definition {
+        cps {
+            script("""
+                pipeline {
+                    agent any
+                    stages {
+                        stage('Build') {
+                            steps {
+                                echo "Running image tag CI pipeline for Uniticonpro..."
+                                // Add your steps here
+                            }
+                        }
+                    }
+                }
+            """.stripIndent())
+            sandbox(true)   // allow running in Groovy sandbox
+        }
+    }
+}
