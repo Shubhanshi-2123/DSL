e33b5018e963f6c8e67034d851575ed9447d1bc2
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-08 13:18:53 +0530

    Update AuditTool.groovy

--- AuditTool.groovy
+++ AuditTool.groovy
@@ -52,18 +52,66 @@
 //     }
 // }
 
+// pipelineJob('AuditTool/ScoutScan') {
+//     description('AWS ScoutSuite security scanning job')
+//     parameters {
+//         stringParam('ROLE_ARN', 'arn:aws:iam::370389955750:role/scout-suite-security-role', 'Enter the Role ARN to assume')
+//         stringParam('ACCOUNT_NAME', 'my-aws-account', 'Name of the AWS account being scanned')
+//         stringParam('EMAIL_RECIPIENT', 'your-email@example.com', 'Email address to send the report to')
+
+//         activeChoiceParam('AWS_REGION') {
+//             description('Select AWS Region')
+//             filterable()
+//             choiceType('SINGLE_SELECT')
+//             groovyScript {
+//                 script('''
+//                     import groovy.json.JsonSlurper
+//                     def url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
+//                     def conn = new URL(url).openConnection()
+//                     conn.setRequestMethod("GET")
+//                     conn.connect()
+//                     if (conn.responseCode != 200) {
+//                         return ["Error fetching regions"]
+//                     }
+//                     def data = new JsonSlurper().parse(conn.inputStream)
+//                     def regions = data.prefixes.collect { it.region }.findAll { it }.unique().sort()
+//                     return regions
+//                 ''')
+//                 fallbackScript('"us-east-1"')
+//             }
+//         }
+//     }
+//     definition {
+//         cps {
+//             script(readFileFromWorkspace('pipelines/scoutscan.groovy'))
+//             sandbox()
+//         }
+//     }
+// }
+
 pipelineJob('AuditTool/ScoutScan') {
     description('AWS ScoutSuite security scanning job')
+
+    // Simple string parameters
     parameters {
         stringParam('ROLE_ARN', 'arn:aws:iam::370389955750:role/scout-suite-security-role', 'Enter the Role ARN to assume')
         stringParam('ACCOUNT_NAME', 'my-aws-account', 'Name of the AWS account being scanned')
         stringParam('EMAIL_RECIPIENT', 'your-email@example.com', 'Email address to send the report to')
+    }
 
-        activeChoiceParam('AWS_REGION') {
+    // Active Choices parameter (using $class for reliability)
+    configure { project ->
+        project / 'properties' / 'hudson.model.ParametersDefinitionProperty' / 'parameterDefinitions' << 'org.biouno.unochoice.CascadeChoiceParameter' {
+            name('AWS_REGION')
             description('Select AWS Region')
-            filterable()
             choiceType('SINGLE_SELECT')
-            groovyScript {
+            filterLength(1)
+            referencedParameters('')
+            script(class: 'org.biouno.unochoice.GroovyScript') {
+                fallbackScript(class: 'org.biouno.unochoice.GroovyScript') {
+                    script('return ["us-east-1"]')
+                    sandbox(true)
+                }
                 script('''
                     import groovy.json.JsonSlurper
                     def url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
@@ -77,10 +125,12 @@ pipelineJob('AuditTool/ScoutScan') {
                     def regions = data.prefixes.collect { it.region }.findAll { it }.unique().sort()
                     return regions
                 ''')
-                fallbackScript('"us-east-1"')
+                sandbox(true)
             }
         }
     }
+
+    // Pipeline definition
     definition {
         cps {
             script(readFileFromWorkspace('pipelines/scoutscan.groovy'))
@@ -88,4 +138,3 @@ pipelineJob('AuditTool/ScoutScan') {
         }
     }
 }
-
