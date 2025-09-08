70416ec05427f9b74823f3fcd2ff2730135fe14d
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-08 09:34:38 +0530

    Update AuditTool.groovy

--- AuditTool.groovy
+++ AuditTool.groovy
@@ -7,7 +7,7 @@ pipelineJob('AuditTool/ScoutScan') {
     description('AWS ScoutSuite security scanning job (with backup & markdown report)')
     definition {
         cps {
-            script(readFileFromWorkspace('DSL/pipelines/scoutscan.groovy'))
+            script(readFileFromWorkspace('pipelines/scoutscan.groovy'))
             sandbox()
         }
     }
