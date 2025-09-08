b08f5da130c600a9eb5705aeb4e52d49463e7ef4
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-08 13:49:34 +0530

    Delete scoutscan.groovy

--- scoutscan.groovy
+++
@@ -1,9 +0,0 @@
-pipelineJob('AuditTool/ScoutScan') {
-    description('AWS ScoutSuite security scanning job (with backup & markdown report)')
-    definition {
-        cps {
-            script(readFileFromWorkspace('pipelines/scoutscan.groovy'))
-            sandbox()
-        }
-    }
-}
