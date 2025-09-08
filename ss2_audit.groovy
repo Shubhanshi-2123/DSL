94987ae7dd4b0a154a0ea12f64a40bfb33deed3d
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-08 14:15:50 +0530

    Update ss2_audit.groovy

--- ss2_audit.groovy
+++ ss2_audit.groovy
@@ -1,23 +1,18 @@
 folder('AuditTool') {
     displayName('AuditTool')
-    description('Folder for all AuditTool jobs')
 }
 
 pipelineJob('AuditTool/ss2') {
-    description('ScoutSuite Scan job for AWS account (ss2)')
 
-    // Top-level properties block ensures parameters appear in UI
+    // String parameters
+    parameters {
+        stringParam('ROLE_ARN', 'arn:aws:iam::370389955750:role/scout-suite-security-role', 'Enter the Role ARN to assume')
+        stringParam('ACCOUNT_NAME', 'my-aws-account', 'Name of the AWS account being scanned')
+        stringParam('EMAIL_RECIPIENT', 'your-email@example.com', 'Email address to send the report to')
+    }
+
+    // Active Choices parameter
     configure { project ->
-
-        // String parameters
-        project / 'properties' / 'hudson.model.ParametersDefinitionProperty' / 'parameterDefinitions' << 
-            stringParam('ROLE_ARN', 'arn:aws:iam::370389955750:role/scout-suite-security-role', 'Enter the Role ARN to assume')
-        project / 'properties' / 'hudson.model.ParametersDefinitionProperty' / 'parameterDefinitions' << 
-            stringParam('ACCOUNT_NAME', 'my-aws-account', 'Name of the AWS account being scanned')
-        project / 'properties' / 'hudson.model.ParametersDefinitionProperty' / 'parameterDefinitions' << 
-            stringParam('EMAIL_RECIPIENT', 'your-email@example.com', 'Email address to send the report to')
-
-        // Active Choices parameter
         project / 'properties' / 'hudson.model.ParametersDefinitionProperty' / 'parameterDefinitions' << 
             'org.biouno.unochoice.CascadeChoiceParameter'(
                 plugin: 'uno-choice@2.5'
@@ -48,11 +43,11 @@ pipelineJob('AuditTool/ss2') {
                 }
             }
 
-        // Optional: prevent concurrent builds
+        // Prevent concurrent builds
         project / 'properties' / 'hudson.model.concurrentBuild.ConcurrentBuildProperty' << {}
     }
 
-    // Pipeline definition: call script from workspace
+    // Pipeline definition
     definition {
         cps {
             script(readFileFromWorkspace('pipelines/ss2.groovy'))
