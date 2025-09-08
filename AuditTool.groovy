a500bd1ec63d78a4582c333d5b5e2a8643b5a81a
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-08 09:49:15 +0530

    Update AuditTool.groovy

--- AuditTool.groovy
+++ AuditTool.groovy
@@ -1,12 +1,52 @@
+// folder('AuditTool') {
+//     description('Folder containing all Scout Suite and audit jobs')
+// }
+
+// pipelineJob('AuditTool/ScoutScan') {
+//     description('AWS ScoutSuite security scanning job (with backup & markdown report)')
+//     definition {
+//         cps {
+//             script(readFileFromWorkspace('pipelines/scoutscan.groovy'))
+//             sandbox()
+//         }
+//     }
+// }
 folder('AuditTool') {
     description('Folder containing all Scout Suite and audit jobs')
 }
 
 pipelineJob('AuditTool/ScoutScan') {
-    description('AWS ScoutSuite security scanning job (with backup & markdown report)')
+    description('AWS ScoutSuite security scanning job')
     definition {
         cps {
             script(readFileFromWorkspace('pipelines/scoutscan.groovy'))
+            sandbox()
+        }
+    }
+}
+
+pipelineJob('AuditTool/scoutscuite-gcp') {
+    description('GCP ScoutSuite security scanning job')
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelines/scoutscuite-gcp.groovy'))
+            sandbox()
+        }
+    }
+}
+pipelineJob('AuditTool/ss') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelines/ss.groovy'))
+            sandbox()
+        }
+    }
+}
+
+pipelineJob('AuditTool/ss2') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelines/ss2.groovy'))
             sandbox()
         }
     }
