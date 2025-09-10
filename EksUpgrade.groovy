8edc888805324f186cfd2c3060cb44eb29330e3e
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-10 15:28:25 +0530

    Update EksUpgrade.groovy

--- EksUpgrade.groovy
+++ EksUpgrade.groovy
@@ -20,38 +20,32 @@ pipelineJob('COE/k8s/Eks-Upgrade/EKS-UPGRADE') {
         daysToKeep(30)
         numToKeep(50)
     }
-    
-   disableConcurrentBuilds()
-    
+
+    // Disable concurrent builds correctly for pipelineJob
+    configure { project ->
+        project / 'properties' / 'hudson.model.concurrent.ConcurrencyThrottleJobProperty' {
+            maxConcurrentPerNode(1)
+            maxConcurrentTotal(1)
+        }
+    }
+
     parameters {
         base64File(name: 'cluster_properties_yamlfile', description: 'Upload your cluster YAML file')
     }
-    
+
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
                         url('git@github.com:OT-COE/shared-library.git')
-                        credentials('jenkins') // ssh-key-OT-COE-org
+                        credentials('jenkins')
                     }
                     branch('*/ot-261-develop-custom-ami-v3')
-                    extensions {
-                        // Optional: lightweight checkout
-                        cloneOptions {
-                            shallow(true)
-                            depth(1)
-                        }
-                    }
                 }
             }
             scriptPath('Jenkinsfile')
             lightweight(true)
         }
-    }
-
-    properties {
-        pipelineTriggers([]) // Add triggers if needed later
-        disableConcurrentBuilds()
     }
 }
