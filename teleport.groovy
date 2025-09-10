84c2bb2991631ade7a926a4c20c11cfd0240e15a
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-10 21:04:25 +0530

    Create teleport.groovy

---
+++ teleport.groovy
@@ -0,0 +1,23 @@
+
+folder('Teleport') {}
+
+
+pipelineJob('Teleport/ansible-role-execution') {
+    displayName('ansible-role-execution')
+    description('Pipeline for executing Ansible roles')
+
+    // Log rotation
+    logRotator {
+        daysToKeep(3)      
+        numToKeep(null)    
+    }
+
+    // Pipeline script (inline)
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelines/ansible-role-execution.groovy')) // adjust path as needed
+            sandbox(true) // Groovy sandbox enabled
+        }
+    }
+
+}
