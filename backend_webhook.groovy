e378abca6d00f4d676f8f0314ee6c7aa9429bd14
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-10 19:50:21 +0530

    Create backend_webhook.groovy

---
+++ backend_webhook.groovy
@@ -0,0 +1,39 @@
+// === Folder Structure ===
+folder('COE') {}
+folder('COE/CI') {}
+folder('COE/CI/SentryFuse') {}
+
+def repoUrl = 'https://github.com/OT-COE/SentryFuse.git'
+def githubCredId = 'sharvarikhamkar1304-creds' // replace with actual credential ID
+
+// === Backend Webhook Multibranch Pipeline ===
+multibranchPipelineJob('COE/CI/SentryFuse/Backend-Webhook') {
+    displayName('Backend-Webhook')
+    description('Multibranch pipeline for Backend Webhook')
+
+    branchSources {
+        github {
+            id('backend-webhook-github') // unique ID
+            repoOwner('OT-COE')
+            repository('SentryFuse')
+            credentialsId(githubCredId)
+            scanCredentialsId(githubCredId)
+            includes('*') // build all branches
+        }
+    }
+
+    orphanedItemStrategy {
+        discardOldItems {
+            daysToKeep(1)
+            numToKeep(1)
+        }
+    }
+
+   
+
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('backend-webhook/Jenkinsfile')
+        }
+    }
+}
