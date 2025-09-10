e33f6191a02ff8ab39c44efe26ccc97e06ddaace
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-10 17:37:30 +0530

    Update Toolhub_helm_CD.groovy

--- Toolhub_helm_CD.groovy
+++ Toolhub_helm_CD.groovy
@@ -1,17 +1,8 @@
 folder('COE') {
     displayName('COE')
-
 }
 
 folder('COE/Toolhub-helm-CD') {
     displayName('Toolhub-helm-CD')
-}
-
-pipelineJob('COE/Toolhub-helm-CD') {
-
- 
-    definition {
-        cps {
-        }
-    }
+   
 }
