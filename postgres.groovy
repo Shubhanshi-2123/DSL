8d3ae67c363dcc6c5e19537ea1cb696f71ba1d12
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-10 20:43:18 +0530

    Create postgres.groovy

---
+++ postgres.groovy
@@ -0,0 +1,16 @@
+// === Folder Structure ===
+folder('COE') {}
+folder('COE/CI') {}
+folder('COE/CI/AI-Tools') {}
+
+// === Postgres-MCP Pipeline ===
+pipelineJob('COE/CI/AI-Tools/Postgres-MCP') {
+    displayName('Postgres-MCP')
+
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelines/postgres.groovy'))
+            sandbox(true) // enable Groovy sandbox
+        }
+    }
+}
