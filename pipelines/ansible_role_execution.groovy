4a0af860c7404ad526b847eda672982911414bb0
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-16 12:41:26 +0530

    Update ansible_role_execution.groovy

--- pipelines/ansible_role_execution.groovy
+++ pipelines/ansible_role_execution.groovy
@@ -1,29 +1,29 @@
-pipeline {
-    agent any
-        stages {
-        stage('Checkout') {
-            steps {
-                git url: 'https://github.com/OT-OSM/teleport.git', branch: 'teleport'
-            }
-        }
+// pipeline {
+//     agent any
+//         stages {
+//         stage('Checkout') {
+//             steps {
+//                 git url: 'https://github.com/OT-OSM/teleport.git', branch: 'teleport'
+//             }
+//         }
 
-        stage('Run Ansible Role') {
-            steps {
-                script {
-                    sh '''
-                        ansible-playbook -i inventory/Teleport-agent.ini playbook.yaml
-                      '''
-                }
-            }
-        }
-    }
+//         stage('Run Ansible Role') {
+//             steps {
+//                 script {
+//                     sh '''
+//                         ansible-playbook -i inventory/Teleport-agent.ini playbook.yaml
+//                       '''
+//                 }
+//             }
+//         }
+//     }
 
-    post {
-        success {
-            echo "Ansible role executed successfully."
-        }
-        failure {
-            echo "Ansible role execution failed."
-        }
-    }
-}
+//     post {
+//         success {
+//             echo "Ansible role executed successfully."
+//         }
+//         failure {
+//             echo "Ansible role execution failed."
+//         }
+//     }
+// }
