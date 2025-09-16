c9fa75f021d932f3e0066aecc0a1dd8e67ac252b
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-09-16 12:45:34 +0530

    Update AuditTool.groovy

--- AuditTool.groovy
+++ AuditTool.groovy
@@ -1,18 +1,18 @@
-// folder('AuditTool') {
-//     displayName('AuditTool')
-// }
+//  folder('AuditTool') {
+//      displayName('AuditTool')
+//  }
 
 
 
-// pipelineJob('AuditTool/ScoutScan') {
+//  pipelineJob('AuditTool/ScoutScan') {
 //     description('AWS ScoutSuite security scanning job')
 
-//     // Simple string parameters
+// //     // Simple string parameters
 //     parameters {
-//         stringParam('ROLE_ARN', 'arn:aws:iam::370389955750:role/scout-suite-security-role', 'Enter the Role ARN to assume')
-//         stringParam('ACCOUNT_NAME', 'my-aws-account', 'Name of the AWS account being scanned')
-//         stringParam('EMAIL_RECIPIENT', 'your-email@example.com', 'Email address to send the report to')
-//     }
+//          stringParam('ROLE_ARN', 'arn:aws:iam::370389955750:role/scout-suite-security-role', 'Enter the Role ARN to assume')
+//          stringParam('ACCOUNT_NAME', 'my-aws-account', 'Name of the AWS account being scanned')
+//          stringParam('EMAIL_RECIPIENT', 'your-email@example.com', 'Email address to send the report to')
+//      }
 
 //     // Active Choices parameter (using $class for reliability)
 //     configure { project ->
